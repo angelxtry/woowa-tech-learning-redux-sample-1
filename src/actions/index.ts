ad0487e675e70ffa2a2b4d23fc36e3ad98804c05
angelxtry <angelxtry@gmail.com>
2020-09-25 15:16:28 +0900

    typesafe-actions 5.x로 변경

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,15 +1,6 @@
 import { createAction } from 'typesafe-actions';
 
-export const startMonitoring = createAction('@command/monitoring/start', (resolve) => () => resolve());
-
-export const stopMonitoring = createAction('@command/monitoring/stop', (resolve) => {
-  return () => resolve();
-});
-
-export const fetchSuccess = createAction('@fetch/success', (resolve) => {
-  return () => resolve();
-});
-
-export const fetchFailure = createAction('@fetch/failure', (resolve) => {
-  return () => resolve();
-});
+export const startMonitoring = createAction('@command/monitoring/start')();
+export const stopMonitoring = createAction('@command/monitoring/stop')();
+export const fetchSuccess = createAction('@fetch/success')();
+export const fetchFailure = createAction('@fetch/failure')();
